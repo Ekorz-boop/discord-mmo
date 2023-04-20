9633435a85b4d3a25f0a9f9189baf746a3616dff
Simon <muckot@gmail.com>
2023-04-20 17:12:47 +0200

    fix error in the run command

--- setup.py
+++ setup.py
@@ -2,13 +2,17 @@ import os
 import sys
 import subprocess
 
+
 def run_command(command):
+    if command.startswith("flask"):
+        command = "python -m " + command
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
     if process.returncode != 0:
         print(f"Error: {stderr.decode('utf-8')}")
     else:
         print(stdout.decode('utf-8'))
+
 
 
 def main():
@@ -29,6 +33,7 @@ def main():
 
     # Start the server
     print("Starting the server...")
+    run_command("set FLASK_APP=discord_mmo_server.py")
     run_command("flask run")
 
 
